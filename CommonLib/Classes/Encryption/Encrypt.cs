ad7a61739a60ae68302e9ccef87f3ddd14a1aa56
Deathpave <49186768+Deathpave@users.noreply.github.com>
2021-09-15 08:17:52 +0200

    Encryption added

---
+++ CommonLib/Classes/Encryption/Encrypt.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLib.Classes.Encryption
+{
+    internal class Encrypt : IDisposable
+    {
+        public void Dispose()
+        {
+            // Null all that can be nulled
+        }
+
+        // Salt byte array
+        byte[] salt = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+        // Creates stream to "write" the data
+        MemoryStream memoryStream = new MemoryStream();
+
+        // Rijndael class
+        RijndaelManaged rijndael = new RijndaelManaged();
+        public string EncryptString(string input, string password)
+        {
+
+            // Derived bytes to make key and vector
+            Rfc2898DeriveBytes rfc = new Rfc2898DeriveBytes(password, salt);
+
+            // Key gets set
+            rijndael.Key = rfc.GetBytes(32);
+
+            // Vector gets set
+            rijndael.IV = rfc.GetBytes(16);
+
+            // Creates stream to "write" the data            
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, rijndael.CreateEncryptor(), CryptoStreamMode.Write);
+
+            // Gets bytes of the input
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+            // Converts the input bytes to encrypted bytes
+            cryptoStream.Write(inputBytes, 0, inputBytes.Length);
+            cryptoStream.FlushFinalBlock();
+            cryptoStream.Close();
+
+            // Converts the encryted bytes back to string
+            string result = Convert.ToBase64String(memoryStream.ToArray());
+            return result;
+        }
+
+        public string EncryptString(string input, string password, int itterations)
+        {
+
+            // Derived bytes to make key and vector
+            Rfc2898DeriveBytes rfc = new Rfc2898DeriveBytes(password, salt, itterations);
+
+            // Key gets set
+            rijndael.Key = rfc.GetBytes(32);
+
+            // Vector gets set
+            rijndael.IV = rfc.GetBytes(16);
+
+            // Creates stream to "write" the data            
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, rijndael.CreateEncryptor(), CryptoStreamMode.Write);
+
+            // Gets bytes of the input
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+            // Converts the input bytes to encrypted bytes
+            cryptoStream.Write(inputBytes, 0, inputBytes.Length);
+            cryptoStream.FlushFinalBlock();
+            cryptoStream.Close();
+
+            // Converts the encryted bytes back to string
+            string result = Convert.ToBase64String(memoryStream.ToArray());
+            return result;
+        }
+    }
+}
---
+++ CommonLib/Classes/Managers/SecurityManager.cs
@@ -0,0 +1,27 @@
+﻿using CommonLib.Classes.Encryption;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLib.Classes.Managers
+{
+    public class SecurityManager
+    {
+        private Encrypt _encrypt;
+
+        public void Test()
+        {
+            // Creates a new instance of encryption classs
+            _encrypt = new Encrypt();
+            //
+            // Do code here
+            //
+            // Disposes of data in encryption class
+            _encrypt.Dispose();
+            // Nulls instance of encryption class
+            _encrypt = null;
+        }
+    }
+}
